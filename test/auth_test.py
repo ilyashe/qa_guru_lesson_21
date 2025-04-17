d267c8d64fad931bbc87410317f6af104c777ad3
ilyashe <ilya.shebanoff@gmail.com>
2025-04-17 12:41:45 +0300

    add auth_test

---
+++ test/auth_test.py
@@ -0,0 +1,14 @@
+import allure
+
+
+
+@allure.id("37473")
+@allure.title("auth via Google(клон)")
+@allure.tag("web", "regress")
+def test_auth_via_google():
+    with allure.step("Step 1"):
+        pass
+    with allure.step("Step 2"):
+        pass
+    with allure.step("Step 3"):
+        pass
